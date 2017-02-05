c0044bd3af342192169d380216efce8a449ceb1d
Quentin Lautischer <lautisch@ualberta.ca>
2017-02-04 23:03:43 -0700

    non-optimized code complete

--- LAB2/main_client.c
+++ LAB2/main_client.c
@@ -1,79 +1,83 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
 
+#define STRMAX 200
+#define NUMTHREADS 20
 
 int port = 0;
 int array_size = 0;
+int* seed;
 
 void *Talk (void *args);
+void Usage (char* prog_name);
 
 int main(int argc, char* argv[])
 {
-	int num_threads = 20;
-	pthread_t thread_handles[num_threads];
-
-
+	pthread_t thread_handles[NUMTHREADS];
 
 	if (argc != 3) Usage(argv[0]);
+
 	port = atoi(argv[1]);
 	array_size = atoi(argv[2]);
 
-	for(int i=0;i<num_threads;i++)      //can support 20 clients at a time
-	{
-		printf("\nCreating Talker %d\n",i);
+	/* Intializes random number generators */
+	seed = malloc(NUMTHREADS*sizeof(int));
+	for (int i = 0; i < NUMTHREADS; i++)
+		seed[i] = i;
+
+	for(int i=0;i<NUMTHREADS;i++) {
 		pthread_create(&thread_handles,NULL,Talk,(void *)i);
 	}
-	for(int i=0;i<num_threads;i++)      //can support 20 clients at a time
-	{
-		printf("\nJoining Talker %d\n",i);
+	printf("Created All Talkers\n");
+
+	for(int i=0;i<NUMTHREADS;i++) {
 		pthread_join(thread_handles[i], NULL);
 	}
-
+	printf("Joined All Talkers\n");
 
 	return 0;
 }
 
-
 void *Talk (void *args) { //port, array_size, rank
-
 	long rank = (long)args;
 
 	struct sockaddr_in sock_var;
 	int clientFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
-	char str_clnt[20],str_ser[20];
+	char str_clnt[STRMAX],str_ser[STRMAX];
 
 	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
 	sock_var.sin_port=port;
 	sock_var.sin_family=AF_INET;
 
-	if(connect(clientFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
-	{
-		printf("Current client is: %d\n",clientFileDescriptor);
-		printf("\nEnter String to send for client %d: ", clientFileDescriptor);
-		fgets(str_clnt, 20, stdin);
-		write(clientFileDescriptor,str_clnt,20);
-		read(clientFileDescriptor,str_ser,20);
-		printf("String from Server: %s\n",str_ser);
+	// Find a random position in theArray for read or write
+	int pos = rand_r(&seed[rank]) % array_size;
+	int randNum = rand_r(&seed[rank]) % 100;	// write with 5% probability
+
+	if(connect(clientFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0) {
+		if (randNum <= 95) { // read request
+			snprintf(str_clnt, STRMAX, "r%d", pos);
+		}
+		else { //write request
+			snprintf(str_clnt, STRMAX, "w%d", pos);
+		}
+	
+		write(clientFileDescriptor,str_clnt,STRMAX);
+		read(clientFileDescriptor,str_ser,STRMAX);
+		printf("%s\n",str_ser);
 		close(clientFileDescriptor);
 	}
-	else{
+	else {
 		printf("socket creation failed");
 	}
 	return 0;
 }
 
-/*------------------------------------------------------------------
- * Function:  Usage
- * Purpose:   print a message showing what the command line should
- *            be, and terminate
- * In arg :   prog_name
- */
 void Usage (char* prog_name) {
     fprintf(stderr, "usage: %s <port number> <array length>\n", prog_name);
     exit(0);
--- LAB2/main_server.c
+++ LAB2/main_server.c
@@ -1,57 +1,98 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "timer.h"
 
-void *ServerEcho(void *args)
-{
-	int clientFileDescriptor=(int)args;
-	char str[20];
+#define STRMAX 200
+#define NUMTHREADS 20
 
-	read(clientFileDescriptor,str,20);
-	printf("\nreading from client %d: %s",clientFileDescriptor,str);
-	write(clientFileDescriptor,str,20);
-	printf("\nechoing back to client %d", clientFileDescriptor);
-	close(clientFileDescriptor);
-}
+int array_size;
+pthread_mutex_t mutex;
+char theArray[1024][STRMAX];
 
+void Usage (char* prog_name);
+void *ServerEcho(void *args);
+void initialize_string_array();
+void print_array();
 
 int main(int argc, char* argv[])
 {
+	double start, finish, elapsed;	
 	struct sockaddr_in sock_var;
 	int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
 	int clientFileDescriptor;
-	int i;
-	pthread_t t[20];
+	pthread_t t[NUMTHREADS];
+	pthread_mutex_init(&mutex, NULL);
 
 	if (argc != 3) Usage(argv[0]);
+	
+	array_size = atoi(argv[2]);
 
 	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
 	sock_var.sin_port=atoi(argv[1]);
 	sock_var.sin_family=AF_INET;
-	if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
-	{
-		printf("\nsocket has been created");
+
+	initialize_string_array();
+	
+	if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0) {
+		printf("\nServer Socket has been created\n");
 		listen(serverFileDescriptor,2000); 
-		while(1)        //loop infinity
-		{
-			for(i=0;i<20;i++)      //can support 20 clients at a time
-			{
+		while(1) {       //loop infinity... why? I think maybe... sure
+			GET_TIME(start); 
+			for(int i=0;i<NUMTHREADS;i++) {
 				clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
-				printf("\nConnected to client %d\n",clientFileDescriptor);
 				pthread_create(&t,NULL,ServerEcho,(void *)clientFileDescriptor);
 			}
+			for(int i=0;i<NUMTHREADS;i++) {
+				pthread_join(t[i], NULL);
+			}
+			GET_TIME(finish);
+
+			print_array();
+			
+			elapsed = finish - start;	
+	 		printf("The elapsed time is %e seconds\n", elapsed);
 		}
 		close(serverFileDescriptor);
 	}
-	else{
-		printf("\nsocket creation failed\n");
+	else {
+		printf("Socket creation failed\n");
 	}
 	return 0;
+}
+
+/*------------------------------------------------------------------
+ * Function:  Usage
+ * Purpose:   print a message showing what the command line should
+ *            be, and terminate
+ * In arg :   prog_name
+ */
+void *ServerEcho(void *args) {
+	int clientFileDescriptor=(int)args;
+	char str[STRMAX];
+
+	read(clientFileDescriptor,str,STRMAX);
+
+	char* pos = str;
+	pos++;
+	int index = atoi(pos);
+
+	pthread_mutex_lock(&mutex); 
+	if (str[0] == 'w') {
+		sprintf(theArray[index], "String %d has been modified by a write request", index);
+		sprintf(str, "%s", theArray[index]);
+	} else {
+		sprintf(str, "%s", theArray[index]);
+	}
+	pthread_mutex_unlock(&mutex);
+
+	write(clientFileDescriptor,str,STRMAX);
+	close(clientFileDescriptor);
 }
 
 /*------------------------------------------------------------------
@@ -64,3 +105,17 @@ void Usage (char* prog_name) {
     fprintf(stderr, "usage: %s <port number> <array length>\n", prog_name);
     exit(0);
 }  /* Usage */
+
+void initialize_string_array() {
+	for (int i = 0; i < array_size; i ++) {
+		sprintf(theArray[i], "String %d: the initial value", i);
+	}
+	print_array();
+}
+
+void print_array(){
+	for (int i = 0; i < array_size; i ++) {
+		printf("%s\n", theArray[i]);
+	}
+}
+
