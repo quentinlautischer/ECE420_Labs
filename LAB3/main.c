98ebe03c1c5abff06c3c4eee46aa6d2ae28653b0
Quentin <lautisch@e5-05-18.ece.ualberta.ca>
2017-03-02 15:42:10 -0700

    sync

--- LAB3/main.c
+++ LAB3/main.c
@@ -20,14 +20,21 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include "timer.h" // For speed testing
-#include "lab3IO.h"
+#include "Lab3IO.h"
+
+
+void Gaussian(double** A, int n);
+void Jordan();
+void CopyMatrix(double **A, double** B, int rows, int cols);
+void SwapRow(double **A, int row1, int row2, int col_size);
 
 /* Global variables */
 int thread_count;
 
 int size;
-int* A;
+double** A;
 
+double *x;
 
 // IO
 // int Lab3LoadInput(double ***A, int *size);
@@ -40,18 +47,22 @@ int* A;
 // int PrintVec(double* b, int size);
 // int DestroyVec(double* b);
 
-void report_timing(double start, double end) {
+double report_timing(double start, double end) {
     GET_TIME(end);
     double total_time = (double) end - start;
     printf("It took %f seconds\n", total_time);
+    return total_time;
 }
+
+
+
 
 /*------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
     double     t_start = 0.0;
     double     t_end = 0.0;
     long       thread;
-    pthread_t* thread_handles;
+    //pthread_t* thread_handles;
     
     if (argc != 2) {
         printf("Get your args together c'mon");
@@ -59,12 +70,15 @@ int main(int argc, char* argv[]) {
     }
     
     thread_count = atoi(argv[1]);
-    thread_handles = malloc(thread_count*sizeof(pthread_t));
+    //thread_handles = malloc(thread_count*sizeof(pthread_t));
     printf("Thread Count: %i\n", thread_count);
 
     //load Matrix
-    double** CreateMat(int NumRow, int NumCol);
-    Lab3LoadInput(double ***A, int *size);
+    A = CreateMat(size, size);
+    x = CreateVec(size);
+    Lab3LoadInput(&A, &size);
+
+    PrintMat(A, size, size);
     
     GET_TIME(t_start);
     
@@ -76,11 +90,17 @@ int main(int argc, char* argv[]) {
         //pthread_join(thread_handles[thread], NULL);
     }
         
-    report_timing(t_start, t_end);
+    double t = report_timing(t_start, t_end);
     
+    Gaussian(A, size);
+    PrintMat(A, size, size);
+
     //Output Matrix
+    Lab3SaveOutput(x, size, t);
 
     //Free Memory
+    DestroyVec(x);
+    DestroyMat(A, size);
     
     return 0;
 }  /* main */
@@ -104,10 +124,50 @@ int main(int argc, char* argv[]) {
     return NULL;
  }  /* Pth_mat_mat */
 
-void Gaussian() {
 
+
+void Gaussian(double** A, int n) {
+    int largest_index = 0;
+    int largest_value = 0;
+
+    for (int k = 0; k < n; k++) {
+        /*Pivoting*/
+        //Search for the row which has the max value in column k.
+        for (int i = k; i < n; i++) {
+            if (A[i][k] > largest_value) {
+                largest_value = A[i][k];
+                largest_index = i;
+            }
+        }
+        // Swap the current row with the row that has the maximum value.
+        SwapRow(A, k, largest_index, n);
+
+        for (int i = k+1; i < n; i++) {
+            double temp = A[i][k]/A[k][k];
+            for (int j = k; j < n; j++) {
+                A[i][j] = A[i][j] - (temp*A[k][j]);
+            }
+        }
+    }
 }
 
 void Jordan() {
 
+}
+
+
+void CopyMatrix(double **A, double** B, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            B[i][j] = A[i][j];
+        }
+    }
+}
+
+void SwapRow(double **A, int row1, int row2, int col_size) {
+    for (int j = 0; j < col_size; j++) {
+        double tmp = A[row1][j];
+        A[row1][j] = A[row2][j];
+        A[row2][j] = tmp;
+    }
 }
